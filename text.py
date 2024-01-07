0bd0f10921fb91e4999f75720b02e2af904e361d
f1r3nz4r <142477436+f1r3nz4r@users.noreply.github.com>
2024-01-07 16:13:24 +0530

    Refactor Patch

---
+++ text.py
@@ -0,0 +1,77 @@
+bannerText = """\n\n\u001b[38;5;196m       ██████    ████  ▄████▄  ███ ▄███▄              /====================\\
+    ▓██   ▒██  ▓██▒██▀ ▀█  ██▄█▒  ▀██▄               / Developed> Smoke-wolf\\
+    ▒████ ▓██  ▒██▒▓█    ▄▓███▄░   ░███▄            /========================\\
+    ░▓█▒  ▓▓█  ░██▒▓▓▄ ▄██▓██ █▄    ████           /|version = v-Installer   |\\
+    ░▒█░  ▒▒█████▓▒ ▓███▀ ▒██▒ █▄  ▄█░███           |________________________|
+     ▒ ░  ░▒▓▒ ▒ ▒░ ░▒ ▒  ▒ ▒▒ ▓▒ ▄█░░████          |release date = 05/01/24 |
+     ░    ░░▒░ ░ ░  ░  ▒  ░ ░▒ ▒░▒███████▒          |MacOS Stable |dd/mm/y   |
+     ░ ░   ░░░ ░ ░░       ░ ░░ ░  ░█████░           |========================|
+           ██▀███ ░▒█████ ░ ██████▓████▄▄▄█████▄▄▄█████▓▄▄▄         
+          ▓██ ▒ ██▒██▒  ██▒██    ▒▓█   ▓  ██▒ ▓▓  ██▒ ▓▒████▄       
+          ▓██ ░▄█ ▒██░  ██░ ▓██▄  ▒███ ▒ ▓██░ ▒▒ ▓██░ ▒▒██  ▀█▄     
+          ▒██▀▀█▄ ▒██   ██░ ▒   ██▒▓█  ░ ▓██▓ ░░ ▓██▓ ░░██▄▄▄▄██    
+          ░██▓ ▒██░ ████▓▒▒██████▒░▒████▒▒██▒ ░  ▒██▒ ░ ▓█   ▓██▒   
+          ░ ▒▓ ░▒▓░ ▒░▒░▒░▒ ▒▓▒ ▒ ░░ ▒░ ░▒ ░░    ▒ ░░   ▒▒   ▓▒█░   
+            ░▒ ░ ▒░ ░ ▒ ▒░░ ░▒  ░ ░░ ░  ░  ░       ░     ▒   ▒▒ ░   
+            ░░   ░░ ░ ░ ▒ ░  ░  ░    ░   ░       ░       ░   ▒      
+             ░    ██████▄▄▄█████▓▒█████ ░███▄    █▓█████     ░  ░   
+                ▒██    ▒▓  ██▒ ▓▒██▒  ██▒██ ▀█   █▓█   ▀            
+                ░ ▓██▄  ▒ ▓██░ ▒▒██░  ██▓██  ▀█ ██▒███              
+                  ▒   ██░ ▓██▓ ░▒██   ██▓██▒  ▐▌██▒▓█  ▄            
+                ▒██████▒▒ ▒██▒ ░░ ████▓▒▒██░   ▓██░▒████▒           
+                ▒ ▒▓▒ ▒ ░ ▒ ░░  ░ ▒░▒░▒░░ ▒░   ▒ ▒░░ ▒░ ░           
+                ░ ░▒  ░ ░   ░     ░ ▒ ▒░░ ░░   ░ ▒░░ ░  ░           
+                ░  ░  ░   ░     ░ ░ ░ ▒    ░   ░ ░   ░              
+                      ░             ░ ░          ░   ░  ░"""
+
+disclaimerText = """
+\033[1;35m---------------------------------------------------------\033[0m
+\033[1;35m                  Rosetta Stone Automation Tool            \033[0m
+\033[1;35m---------------------------------------------------------\033[0m
+
+Rosetta Stone, a language learning platform, comes with a few frustrating aspects. This script aims to streamline 
+and automate the process of completing writing exercises. Specifically, it offers autocomplete functionality for 
+French (FR) Rosetta Stone writing lessons. Please note, this program is tailored for macOS users. 
+
+Using this program implies your acceptance of full responsibility for any outcomes or repercussions.
+
+\033[1;33mAdditional Information:\033[0m This automation tool has been crafted to enhance the user experience 
+when tackling writing tasks on Rosetta Stone. Its functionality aims to save time and effort, providing a 
+smoother learning journey. Please be mindful that while it aids in task completion, it's important to use it 
+responsibly. 
+
+\033[1;34mDisclaimer:\033[0m The usage of this script signifies your acknowledgment of assuming complete 
+accountability for any consequences that may arise from its usage. 
+
+\033[1;31mWARNING:\033[0m Ensure ethical and responsible usage. Overdependence on automated tools may hinder 
+genuine learning experiences and personal development. 
+                    \033[1;33;40mHit `\033[1;34;40menter\033[1;33;40m` to Acknowledge\033[0m
+                    """
+
+menuText = """\033[1;35m+--------------------------- Menu ---------------------------+\033[0m
+\033[1;35m|\033[0m                                                            \033[1;35m|\033[0m
+\033[1;35m|\033[0m  \033[1;33m 0 - Update \033[0m                                              \033[1;35m|\033[0m
+\033[1;35m|\033[0m      Update the tool to the latest version.                \033[1;35m|\033[0m
+\033[1;35m|\033[0m                                                            \033[1;35m|\033[0m
+\033[1;35m|\033[0m  \033[1;33m 1 - Launch Typewriter \033[0m                                   \033[1;35m|\033[0m
+\033[1;35m|\033[0m      Start the automatic typing tool for Rosetta Stone.    \033[1;35m|\033[0m
+\033[1;35m|\033[0m                                                            \033[1;35m|\033[0m
+\033[1;35m|\033[0m  \033[1;33m 2 - Show Support for My Work \033[0m                            \033[1;35m|\033[0m
+\033[1;35m|\033[0m      Create an account with GHPM and gain access to other  \033[1;35m|\033[0m
+\033[1;35m|\033[0m      Rosetta Stone Completion tools, and automation        \033[1;35m|\033[0m
+\033[1;35m|\033[0m      scripts, as well as a astounding package manager      \033[1;35m|\033[0m
+\033[1;35m|\033[0m                                                            \033[1;35m|\033[0m
+\033[1;35m|\033[0m  \033[1;33m 3 - Launch Main FRS modules \033[0m                             \033[1;35m|\033[0m
+\033[1;35m|\033[0m      Launch the Live Assist portion for easy automation    \033[1;35m|\033[0m
+\033[1;35m|\033[0m      of Rosetta Stone                                      \033[1;35m|\033[0m
+\033[1;35m|\033[0m                                                            \033[1;35m|\033[0m
+\033[1;35m|\033[0m  \033[1;33m 4 - Exit \033[0m                                                \033[1;35m|\033[0m
+\033[1;35m|\033[0m      Exit the program.                                     \033[1;35m|\033[0m
+\033[1;35m|\033[0m                                                            \033[1;35m|\033[0m
+\033[1;35m+-----------------------------------------------------------+\033[0m
+"""
+
+downloadMenuText = """\033[1;35m--------------------------------- Login 2 Download Menu ---------------------------------\033[0m
+        \033[1;33m1 - Login to GHPM to Download \033[1;37m\033[1mRosetta Stone Writing Automator\033[0m
+        \033[1;33m0 - Back-Out and Remove FRS\033[0m
+"""
